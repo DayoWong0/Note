c5b58705a7db2bc4ec61dd88e10d4d8a7c568c07
test123 <zz15698>
2020-12-08 22:37:08 +0800

    2020/12/08

---
+++ cs/大三上/1/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    char m[20] = {'0','1','2','3','4','5','6','7','8'};
+
+    // int a = m[0] - '0';
+    int a = (int) m[0];
+
+    // printf("%c", m[0]);
+    
+    printf("%c", a);
+
+    // printf("%d", a);
+}
